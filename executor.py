55a4f9d13b88f0f12cb915aef96558e5299c1fa1
Alexander Kapitonov <notsnake@gmail.com>
2018-04-23 09:16:48 +0300

    added readme, comments

--- executor.py
+++ executor.py
@@ -14,6 +14,7 @@ COUNT_XML_IN_ARCHIVE = 100
 
 
 def create_xml_file():
+    """Create one xml file in memory and return it."""
     root = ET.Element('root')
     ET.SubElement(root, 'var', attrib={'name': 'id', 'value': str(uuid.uuid4())})
     ET.SubElement(root, 'var', attrib={'name': 'level', 'value': str(randint(1, 99))})
@@ -24,6 +25,7 @@ def create_xml_file():
 
 
 def create_zip(filename):
+    """Create one zip archive with xml files."""
     with zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED) as arch:
         for j in range(1, COUNT_XML_IN_ARCHIVE + 1):
             xml_file = create_xml_file()
@@ -31,7 +33,8 @@ def create_zip(filename):
             arch.writestr(xml_filename, xml_file)
 
 
-def create_zips(count=50):
+def create_path_zips(count=50):
+    """Create paths for archives and return them."""
     zips = []
     for i in range(1, count + 1):
         arch_filename = 'archive_{}.zip'.format(i)
@@ -41,6 +44,7 @@ def create_zips(count=50):
 
 
 def process_zip(filename, queue_file_levels, queue_file_objects):
+    """Process a zip."""
     with zipfile.ZipFile(filename, 'r') as arch:
         xml_list = arch.namelist()
         for xml in xml_list:
@@ -55,6 +59,7 @@ def process_zip(filename, queue_file_levels, queue_file_objects):
 
 
 def search_zips():
+    """Return path's arhives for processing."""
     directory = pathlib.Path(ARCHIVES_DIR_NAME)
     if not directory.exists():
         raise Exception('No directory with data.')
@@ -62,6 +67,7 @@ def search_zips():
 
 
 def csv_writer_levels(queue):
+    """Worker is writing to csv file levels."""
     with open(FILENAME_CSV_LEVELS, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         while True:
@@ -72,6 +78,7 @@ def csv_writer_levels(queue):
 
 
 def csv_writer_objects(queue):
+    """Worker is writing to csv file objects."""
     with open(FILENAME_CSV_OBJECTS, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         while True:
@@ -82,8 +89,9 @@ def csv_writer_objects(queue):
 
 
 def start_task_create_zips():
+    """Start creating zips task."""
     pathlib.Path(ARCHIVES_DIR_NAME).mkdir(parents=True, exist_ok=True)
-    archives = create_zips()
+    archives = create_path_zips()
     pool = Pool()
     pool.map(create_zip, archives)
     pool.close()
@@ -91,6 +99,7 @@ def start_task_create_zips():
 
 
 def start_task_process_zips():
+    """Start processing zips task."""
     zips = search_zips()
     manager = Manager()
     queue_file_levels = manager.Queue()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Working with xml and zip files
+Python version: 3
+### Program makes two tasks:
+1. Create several archives with xml files.
+2. Process this archives and make two files with result data.
